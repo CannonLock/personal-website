16262b27745fdbfd2a92a7cd42739cbfdcdc5035
Cannon Lock <lockcannon@hotmail.com>
2020-11-07 22:30:17 -0600

    Swimmer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import { Box, Flex, Heading } from "rebass"
+import { Box, Flex, Heading, Text } from "rebass"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import Swimmer from "../images/WideAlphaRightSize.gif"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
@@ -29,6 +30,7 @@ const HeadShotImage = () => {
         pb: "20px",
         mb: "auto",
         minWidth: ["180px", "250px"],
+        width: ["180px", "250px"], 
         height: "auto",
         borderRadius: 30,
       }}
@@ -41,36 +43,69 @@ const HeadShotImage = () => {
   )
 }
 
-const IndexPage = () => (
+const SwimBoy = () => {
+  const [visible, setVisible] = useState("visible");
+
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        height: "auto",
+        width: "auto",
+        zIndex: 1000,
+        visibility: visible,
+        left: ["-30px", "-30px", "-10px", "-20px"]
+      }}
+      onClick={() => setVisible("hidden")}
+    >
+      <img src={Swimmer} />
+    </Box>
+  )
+}
+
+const IndexPage = ( props ) => (
   <Layout>
     <SEO title="Home" />
+    <SwimBoy/>
     <Flex
-      sx={{flexDirection: "row"}}
+      sx={{
+        flexDirection: "row",
+        position: "relative"
+      }}
     >
       <Box
         sx={{
           mt: [0, 2]
         }}
       >
-        <Heading>Welcome</Heading>
         <Box
          sx={{
            mr: 1
          }}
         >
+          <Heading>Welcome</Heading>
           <p>
-            This website is a work in progress with the intentions of practicing responsive web design and also showing off some of my projects. 
+            This website is a constant work in progress with the intentions of practicing responsive web design and also showing off some of my projects. As you can see on the queue there is alot left to do but that is life.
           </p>
           <p>
-            If you are interested in seeing some of my academic projects click on the Project header. If you are interested in me as a person hop onto interests.
+            Page To-Do Queue:
           </p>
-          <p>
-            If you are feeling crazy hop into dev mode and adjust the screen size for a couple devices or toggle your os darkmode.
-          </p>
+          <ul>
+            <li>
+              <Text sx={{textDecoration:"line-through"}}>Make Breast Stroke Animation</Text>
+            </li>
+            <li>
+              About
+            </li>
+            <li>
+              Projects
+            </li>
+            <li>
+              Interests
+            </li>
+          </ul>
         </Box>
-        
       </Box>  
-      <HeadShotImage/>
     </Flex>
   </Layout>
 )
